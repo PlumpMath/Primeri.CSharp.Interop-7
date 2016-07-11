b1b2ffddad253679efe9625cf4cf7a4a49b63663
Diana Siteva <dianasiteva@hotmail.com>
2016-07-11 15:18:49 +0300

    Модул 09, Видео 05 - дефиниране на библиотеки на excel

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -1,15 +1,66 @@
 ﻿using System;
+using InteropEcel = Microsoft.Office.Interop.Excel;
+
 namespace Excel
 {
 	public class IOWrite
 	{
 
 		private DataStruct _data;
+		private InteropEcel.Application excel;
 
 
 		public IOWrite( DataStruct data )
 		{
 		}
+
+		public bool exportTable()
+		{
+			try
+			{
+				//Междинни проверки
+
+				return true;
+			}
+			catch
+			{
+			}
+			return false;
+		}
+
+		public void addRow(DataRow _row)
+		{
+			try
+			{
+
+
+
+			}
+			catch
+			{
+			}
+
+		}
+
+
+		private void runFile()
+		{
+			try
+			{
+				System.Diagnostics.Process.Start(getPath());
+			}
+			catch
+			{
+			}
+		}
+
+
+		private string getPath()
+		{
+			return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Table1.xlsx");
+		}
+
+
 	}
 }
 
