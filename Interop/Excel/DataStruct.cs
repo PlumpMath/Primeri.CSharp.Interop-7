e539d1afb5ea626b31d2fafecf165cb258b490f7
Diana Siteva <dianasiteva@hotmail.com>
2016-07-11 12:20:49 +0300

    Модул 09, Видео 03 - добавяне на клас за данни

---
+++ Interop/Excel/DataStruct.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Excel
+{
+	public class DataStruct
+	{
+		public List<DataRow> table = new List<DataRow>();
+
+		public DataStruct()
+		{
+		}
+
+		public void addRow(string _fname, string _lname, string _age)
+		{
+			table.Add( new DataRow(_fname,_lname,_age) );
+		}
+
+		public void printTable()
+		{
+			try
+			{
+
+				foreach ( DataRow row in table )
+				{
+					Console.WriteLine( row.firstName +" "+ row.lastName +", "+ row.age );
+				}
+
+			}
+			catch
+			{
+			}
+
+		}
+
+	}
+
+	public class DataRow
+	{
+		private string _firstName = "";
+		private string _lastName = "";
+		private string _age = "";
+
+		public DataRow(string __firstNme, string __lastNme, string __age)
+		{
+			_firstName = __firstNme;
+			_lastName = __lastNme;
+			_age = __age;
+		}
+
+		public string firstName
+		{
+			set { _firstName = value; }
+			get { return _firstName;}
+		}
+
+		public string lastName
+		{
+			set { _lastName = value; }
+			get { return _lastName; }
+		}
+
+		public string age
+		{
+			set { _age = value; }
+			get { return _age; }
+		}
+	}
+
+
+}
+
--- Interop/Excel/Program.cs
+++ Interop/Excel/Program.cs
@@ -6,7 +6,17 @@ namespace Excel
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+
+			DataStruct data = new DataStruct();
+			IOWrite write = new IOWrite( data );
+
+			//Набиране на данни в основната таблица
+			data.addRow("Диана","Ситева","58");
+			data.addRow("Лилия", "Дацева", "33");
+
+			//Проверка на таблицата
+			data.printTable();
+			
 		}
 	}
 }
